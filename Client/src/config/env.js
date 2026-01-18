fcb6c50cca36362555c4d4ff24e8631cd11356c6
rahulkr2511 <krahulrajank@gmail.com>
2026-01-18 14:50:32 +0530

    updated

--- Client/src/config/env.js
+++ Client/src/config/env.js
@@ -3,6 +3,7 @@ export const API_BASE_URL =
 
 export const WS_BASE_URL = process.env.REACT_APP_WS_BASE_URL || 'http://localhost:8080/ws';
 
+// Update the VAPID public key to your own
 export const VAPID_PUBLIC_KEY =
   process.env.REACT_APP_VAPID_PUBLIC_KEY ||
   'BLvt0WNCKhjo54kvt4q0k85sBJnXVRaNhg2XKIrztma3FgoFfJrnLZY0gBg6U2f2KD-86EYAfrvg7Dt1VWuPxJI';
